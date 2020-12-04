7460aa13a0db45a9944acb3f40441dd97da72c0a
Michał Anioł <michal.janusz.aniol@gmail.com>
2020-12-04 04:14:36 +0100

    init

---
+++ dom.js
@@ -0,0 +1,16 @@
+const dom = (function() {
+
+    // #region [rgba(0,0,120,0.1)] --> OBSŁUGA PÓL INPUTÓW
+
+
+
+    // #endregion
+
+    // #region [rgba(255,0,255,0.05)] --> URUCHOMIENIE MODUŁU
+
+    return {
+
+    }
+
+    // #endregion
+}())
